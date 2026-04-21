62fc46f75b85104bef40fbf36264b09245d12f86
David Birota <davidbirota@MacBook-Pro-de-David.local>
2026-04-21 10:03:28 +0400

    Désactivation de l'optimisation des polices

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // On désactive l'optimisation des polices pour éviter le crash serveur
+  experimental: {
+    optimizeFonts: false,
+  },
 };
 
 export default nextConfig;
