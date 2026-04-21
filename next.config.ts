c4e122051b01b77f45d2edec1ea50de4c658b465
David Birota <davidbirota@MacBook-Pro-de-David.local>
2026-04-21 09:51:17 +0400

    Correction du conflit dans next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,8 @@
-<<<<<<< HEAD
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-=======
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
->>>>>>> d2eb0c072d619ed0d85e4171f70d6ecbba446d59
+  reactStrictMode: true,
 };
 
 export default nextConfig;
